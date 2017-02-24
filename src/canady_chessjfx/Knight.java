4568143f186f4b1f2164e8cb4c5cc3b5dd48393c
Rhys Canady <rhysdaniel064@gmail.com>
2017-02-24 09:16:17 -0600

    009 knight piececheck in progress

--- src/canady_chessjfx/Knight.java
+++ src/canady_chessjfx/Knight.java
@@ -43,24 +43,94 @@ public class Knight extends Piece {
 
     @Override
     public void display(Button[][] bbb) {
+        boolean[][] ppp = new boolean[2][8];
+        for (Piece p : Canady_ChessJFX.wlist) {
+            if (p.getX() == this.x - 2 && p.getY() == this.y - 1) {
+                ppp[0][0] = false;
+            }
+            if (p.getX() == this.x - 2 && p.getY() == this.y + 1) {
+                ppp[0][1] = false;
+            }
+            if (p.getX() == this.x + 2 && p.getY() == this.y - 1) {
+                ppp[0][2] = false;
+            }
+            if (p.getX() == this.x + 2 && p.getY() == this.y + 1) {
+                ppp[0][3] = false;
+            }
+            if (p.getX() == this.x - 1 && p.getY() == this.y - 2) {
+                ppp[0][4] = false;
+            }
+            if (p.getX() == this.x + 1 && p.getY() == this.y - 2) {
+                ppp[0][5] = false;
+            }
+            if (p.getX() == this.x - 1 && p.getY() == this.y + 2) {
+                ppp[0][6] = false;
+            }
+            if (p.getX() == this.x + 1 && p.getY() == this.y + 2) {
+                ppp[0][7] = false;
+            }
+        }
+        for (Piece p : Canady_ChessJFX.blist) {
+            if (p.getX() == this.x - 2 && p.getY() == this.y - 1) {
+                ppp[1][0] = false;
+            }
+            if (p.getX() == this.x - 2 && p.getY() == this.y + 1) {
+                ppp[1][1] = false;
+            }
+            if (p.getX() == this.x + 2 && p.getY() == this.y - 1) {
+                ppp[1][2] = false;
+            }
+            if (p.getX() == this.x + 2 && p.getY() == this.y + 1) {
+                ppp[1][3] = false;
+            }
+            if (p.getX() == this.x - 1 && p.getY() == this.y - 2) {
+                ppp[1][4] = false;
+            }
+            if (p.getX() == this.x + 1 && p.getY() == this.y - 2) {
+                ppp[1][5] = false;
+            }
+            if (p.getX() == this.x - 1 && p.getY() == this.y + 2) {
+                ppp[1][6] = false;
+            }
+            if (p.getX() == this.x + 1 && p.getY() == this.y + 2) {
+                ppp[1][7] = false;
+            }
+        }
+
         if (this.x > 1) { // top ones
-            bbb[this.x - 2][this.y - 1].setText("o");
-            bbb[this.x - 2][this.y + 1].setText("o");
+            if (this.y > 0 && ppp[0][0] && ppp[1][0]) {
+                bbb[this.x - 2][this.y - 1].setText("o");
+            }
+            if (this.y < 7 && ppp[0][1] && ppp[1][1]) {
+                bbb[this.x - 2][this.y + 1].setText("o");
+            }
         }
 
         if (this.x < 6) { // bottom ones
-            bbb[this.x + 2][this.y - 1].setText("o");
-            bbb[this.x + 2][this.y + 1].setText("o");
+            if (this.y > 0 && ppp[0][2] && ppp[1][2]) {
+                bbb[this.x + 2][this.y - 1].setText("o");
+            }
+            if (this.y < 7 && ppp[0][3] && ppp[1][3]) {
+                bbb[this.x + 2][this.y + 1].setText("o");
+            }
         }
 
         if (this.y > 1) { // left ones
-            bbb[this.x - 1][this.y - 2].setText("o");
-            bbb[this.x + 1][this.y - 2].setText("o");
+            if (this.x > 0 && ppp[0][4] && ppp[1][4]) {
+                bbb[this.x - 1][this.y - 2].setText("o");
+            }
+            if (this.x < 7 && ppp[0][5] && ppp[1][5]) {
+                bbb[this.x + 1][this.y - 2].setText("o");
+            }
         }
 
         if (this.y < 6) { // right ones
-            bbb[this.x - 1][this.y + 2].setText("o");
-            bbb[this.x + 1][this.y + 2].setText("o");
+            if (this.x > 0 && ppp[0][6] && ppp[1][6]) {
+                bbb[this.x - 1][this.y + 2].setText("o");
+            }
+            if (this.x < 7 && ppp[0][7] && ppp[1][7]) {
+                bbb[this.x + 1][this.y + 2].setText("o");
+            }
         }
     }
 }
